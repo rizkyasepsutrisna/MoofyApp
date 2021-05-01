f94145a09339f8be2a7f1d6e48e543fb6362eaaf
Rizky Asep Sutrisna <rizkyasepsutrisna@gmail.com>
2021-05-01 20:53:00 +0700

    Commit Add Layout Login and Register

---
+++ app/src/main/java/com/example/moofyapp/ui/LoginActivity.java
@@ -0,0 +1,27 @@
+package com.example.moofyapp.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.moofyapp.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //Changing Status Bar Color Icon
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        }
+        setContentView(R.layout.activity_login);
+    }
+    public void onLoginClick(View view) {
+        startActivity(new Intent(this, RegisterActivity.class));
+        overridePendingTransition(R.anim.slide_in_right, R.anim.stay);
+    }
+}
