849c3119823a1e6dfcd3caa7a6a6f0315cc93e95
Rizky Asep Sutrisna <rizkyasepsutrisna@gmail.com>
2021-05-03 22:44:02 +0700

    Commit Function Login

--- app/src/main/java/com/example/moofyapp/ui/LoginActivity.java
+++ app/src/main/java/com/example/moofyapp/ui/LoginActivity.java
@@ -6,20 +6,38 @@ import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.example.moofyapp.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private Button button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        button = (Button) findViewById(R.id.cirLoginButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMainActivity();
+            }
+        });
+
         //Changing Status Bar Color Icon
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
         }
-        setContentView(R.layout.activity_login);
+
     }
+
+    private void openMainActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
     public void onLoginClick(View view) {
         startActivity(new Intent(this, RegisterActivity.class));
         overridePendingTransition(R.anim.slide_in_right, R.anim.stay);
